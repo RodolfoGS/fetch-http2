3fbec6249f6882efb46b60aa94ef35091ade84ad
Rodolfo Gomez Sirimarco <im@rodolfo.gs>
2023-12-08 18:27:20 -0600

    Fixed ping when the session has beed destroyed

--- src/http2.ts
+++ src/http2.ts
@@ -96,7 +96,13 @@ function httpClient(origin: string, options: { pingInterval: number }): ClientHt
 
   // Send a ping every to keep client alive
   if (options.pingInterval > 0) {
-    timer = setInterval(() => client.ping(noop), options.pingInterval).unref()
+    timer = setInterval(() => {
+      if (client.closed || client.destroyed) {
+        closeClient()
+        return
+      }
+      client.ping(noop)
+    }, options.pingInterval).unref()
   }
 
   // Create function to destroy client
@@ -118,7 +124,9 @@ function httpClient(origin: string, options: { pingInterval: number }): ClientHt
 
 function destroyClient(client: ClientHttp2Session, origin: string) {
   // Remove the client from cache
-  clientCache[origin] = undefined
+  if (clientCache[origin] === client) {
+    clientCache[origin] = undefined
+  }
 
   // Close the client
   if (client.closed !== true) {
